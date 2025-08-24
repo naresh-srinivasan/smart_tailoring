d7beeabaf2c763ff6091f843dc9b80c375dad4f4
naresh-srinivasan <say.naresh135@gmail.com>
2025-08-24 13:58:17 +0530

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}", // This pattern covers all your files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
